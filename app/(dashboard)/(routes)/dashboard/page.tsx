66dc55b12eaa12ca390d8fc3a720b6c920e5c0ef
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2024-02-14 16:34:11 +0200

    Add clerk/nextjs

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,8 +1,10 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 export default function DashboardPage() {
   return (
-  <div className="">
-    <p>Dashboard Page</p>
-  </div>
+    <div className="">
+      <p>Dashboard Page</p>
+      <UserButton afterSignOutUrl="/"/>
+    </div>
   );
 }
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import React from "react";
 
 const LandingPage = () => {
   return (
     <div>
-      landing  page (Protected)
+      landing page (Unprotected)
+      <div className="">
+        <Link href="sing-in">
+          <Button>Login</Button>
+        </Link>
+        <Link href="sign-up">
+          <Button>Register</Button>
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;
